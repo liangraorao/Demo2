2a299cfcd8a58dadfdfb92e33f66accd4c26b527
liangraorao <aiworaorao@163.com>
2017-08-02 10:37:29 +0800

    first commit

---
+++ src/main/java/com/songqin/maven/Demo2/App.java
@@ -0,0 +1,13 @@
+package com.songqin.maven.Demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
