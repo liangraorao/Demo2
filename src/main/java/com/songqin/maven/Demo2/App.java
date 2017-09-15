9d5339a8e403042bfe301686504ff964bbf408c5
liangraorao <aiworaorao@163.com>
2017-09-15 11:04:33 +0800

    third time

--- src/main/java/com/songqin/maven/Demo2/App.java
+++ src/main/java/com/songqin/maven/Demo2/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("This is first jenkins demo");
         System.out.println("this is second jenkins test");
+        System.out.println("this is third jenkins test");
     }
 }
