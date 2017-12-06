b98e71476573b80fa505e7b8f1e336250a91bfe4
liangraorao <aiworaorao@163.com>
2017-12-06 15:30:03 +0800

    1206

--- src/main/java/com/songqin/maven/Demo2/App.java
+++ src/main/java/com/songqin/maven/Demo2/App.java
@@ -12,6 +12,6 @@ public class App
         System.out.println("This is first jenkins demo");
         System.out.println("this is second jenkins test");
         System.out.println("this is third jenkins test");
-        System.out.println("this is forth jenkins test");
+        System.out.println("this is fifth jenkins test");
     }
 }
